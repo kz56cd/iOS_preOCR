45767a192f0062de1059b1f45f93cbcddecc799f
kz56cd <kz56cd@gmail.com>
2022-09-03 11:39:32 +0900

    Init project

---
+++ preOCR/preOCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  preOCRApp.swift
+//  preOCR
+//
+//  Created by Masakazu Sano on 2022/09/03.
+//
+
+import SwiftUI
+
+@main
+struct preOCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
